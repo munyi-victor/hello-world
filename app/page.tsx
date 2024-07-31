cd5893a55b316a401e939d8f5f0609c5350517fb
Victor Munyi <munyivictor6@gmail.com>
2024-07-31 16:48:20 +0300

    make navbar for small screen view

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      Hello
+      <Image src="/hw-header.jpeg" alt="" height={600} width={800} />
     </div>
-  )
+  );
 }
 
 export default Home
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,44 @@
-import { CircleEllipsis } from "lucide-react"
+import { CircleEllipsis, Phone } from "lucide-react";
+import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Navbar = () => {
   return (
-    <div>
-      <CircleEllipsis/>
+    <div className="flex text-center text-white items-center justify-around bg-purple-800 h-10 shadow-xl">
+      <div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <CircleEllipsis />
+          </SheetTrigger>
+          <SheetContent side="left" className="w-[250px]">
+            <SheetHeader></SheetHeader>
+            <div>
+              <ul>
+                <li>
+                  <Link href={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link href={"/about"}>About us</Link>
+                </li>
+                <li>
+                  <Link href={"/contact"}>Contact us</Link>
+                </li>
+              </ul>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div>Hello World</div>
+      <Link href={"tel:+254708905136"}>
+        <Phone />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
